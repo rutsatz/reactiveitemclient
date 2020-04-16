29d0e64eededaa9b82232bcf4281a85b2dec7557
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-04-15 22:18:12 -0300

    72. Delete call using WebClient.

--- src/main/java/com/learnreactivespring/controller/ItemClientController.java
+++ src/main/java/com/learnreactivespring/controller/ItemClientController.java
@@ -79,5 +79,12 @@ public class ItemClientController {
                 .log("Updated Item is : ");
     }
 
+    @DeleteMapping("/client/deleteItem/{id}")
+    public Mono<Void> deleteItem(@PathVariable String id) {
+        return webClient.delete().uri("/v1/items/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .log("Deleted Item is ");
+    }
 
 }
